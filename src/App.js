db92e99b81dab0bde7144895a7a3cf5cf851fe59
Ryan <leeyuenhigh@gmail.com>
2021-01-20 20:53:48 -0500

    Added nav to use Link and Route for homepage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
+import { Route, Link } from 'react-router-dom'
+import './App.css';
+import HomePage from './home-page/home-page'
 
-function App() {
-  return (
-    <main className='App'>
-      {/* content goes here */}
-    </main>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state ={
+
+    }
+  }
+
+  render(){
+    return(
+      <>
+        <nav role="navigation">
+          <Link to="/">Home</Link>
+          <Link to= '/card-recommender'>Card Recommendation Tool</Link>
+          <Link to= '/articles'>View Articles</Link>
+          <Link to= '/your-cards'>View Your Cards</Link>
+          <button><Link to="/sign-in">Sign In</Link></button>
+          <button><Link to="/sign-up">Sign Up</Link></button>
+        </nav>
+        <Route
+          exact path = '/'
+          component ={HomePage}
+        />
+      </>
+    )
+  }
 }
 
 export default App;
