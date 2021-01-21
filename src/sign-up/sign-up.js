9e66ed0e7ac96ca08813612db8e61a3e96694a84
Ryan <leeyuenhigh@gmail.com>
2021-01-21 16:05:13 -0500

    Added update functions

--- src/sign-up/sign-up.js
+++ src/sign-up/sign-up.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import './sign-up.css'
 import RecommenderContext from './../recommenderContext'
+const bcrypt = require('bcryptjs')
 
 export default class SignUp extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-
+      username: {
+        value: '',
+        touched: false,
+      },
+      password: {
+        value: '',
+        touched: false,
+      }
     }
   }
 
   static contextType = RecommenderContext;
+
+  updateUserName(userName){
+    this.setState({userName:{value:'',touched:true}})
+  }
+
+  updatePassword(password){
+    this.setState({password:{value:'',touched:true}})
+  }
 
   render(){
     return(
