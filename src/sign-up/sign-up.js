42947756f970f5aa0759af76adcd1f09168c4252
Ryan <leeyuenhigh@gmail.com>
2021-01-22 05:30:20 -0500

    Added validation for email and password

--- src/sign-up/sign-up.js
+++ src/sign-up/sign-up.js
@@ -1,13 +1,15 @@
 import React from 'react'
 import './sign-up.css'
 import RecommenderContext from './../recommenderContext'
-const bcrypt = require('bcryptjs')
+import ValidationError from './../validationError'
+import bcrypt from 'bcryptjs'
+import validator from 'validator';
 
 export default class SignUp extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      username: {
+      email: {
         value: '',
         touched: false,
       },
@@ -20,12 +22,30 @@ export default class SignUp extends React.Component{
 
   static contextType = RecommenderContext;
 
-  updateUserName(userName){
-    this.setState({userName:{value:'',touched:true}})
+  updateEmail(email){
+    this.setState({email:{value:email,touched:true}})
   }
 
   updatePassword(password){
-    this.setState({password:{value:'',touched:true}})
+    this.setState({password:{value:password,touched:true}})
+  }
+
+  validateEmail(){
+    const email = this.state.email.value.trim();
+    if(email.length === 0){
+      return 'Email is required'
+    }else if(!validator.isEmail(email)){
+      return 'Please enter a valid email address'
+    }
+  }
+
+  validatePassword(){
+    const password = this.state.password.value.trim();
+    if(password.length === 0){
+      return 'Password is required'
+    }else if(!validator.isStrongPassword(password)){
+      return 'Password does not meet the above requirements'
+    }
   }
 
   render(){
@@ -33,14 +53,40 @@ export default class SignUp extends React.Component{
       <>
         <form class='signup-form'>
           <div>
-            <label for="username">Email</label>
-            <input type="text" name='username' id='username' />
+            <label for="email">Email</label>
+            <br/>
+            <input 
+              type="text" 
+              name='email' 
+              id='email' 
+              placeholder='Enter your email'
+              onChange={e=>this.updateEmail(e.target.value)}
+            />
+            {this.state.email.touched && (<ValidationError message = {this.validateEmail()}/>)}
           </div>
           <div>
             <label for="password">Password</label>
-            <input type="password" name='password' id='password' />
+            <br></br>
+            <label for="password">Password must be at least 8 characters containing atleast 1 lowercase, 1 uppercase, 1 number, and 1 symbol</label>
+            <br/>
+            <input 
+              type="password" 
+              name='password' 
+              id='password'
+              placeholder='Enter a strong password'
+              onChange={e=>this.updatePassword(e.target.value)}
+            />
+            {this.state.password.touched && (<ValidationError message = {this.validatePassword()}/>)}
           </div>
-          <button type='submit'>Sign Up</button>
+          <button 
+            type='submit'
+            disabled={
+              this.validateEmail()||
+              this.validatePassword
+            }
+          >
+            Sign Up
+          </button>
         </form>
       </>
     )
