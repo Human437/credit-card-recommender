98611d0378a1083727facbf6a1061be308c609f9
Ryan <leeyuenhigh@gmail.com>
2021-01-25 00:52:05 -0500

    Added disclaimer

--- src/card-recommender/card-recommender.js
+++ src/card-recommender/card-recommender.js
@@ -15,43 +15,50 @@ export default class CardRecommender extends React.Component{
   render(){
     return(
       <>
+      <h1 id = 'disclaimer'>
+        Disclaimer: This tool is not meant as a replacement for your own common sense or knowledge of your current needs and overall goals in the credit card game. This tool is only meant to help you make an educated choice for your next card. It should not be the only deciding factor for getting your next credit card.
+      </h1>
         <form id="record-dream">
           <section className="form-section overview-section">
-            <label for="dream-title">Credit Score</label>
-            <p>If you don't know your credit score you can find it by using <a href="https://www.creditkarma.com/">credit karma</a> or looking at the score provided by your issuer. This is necessary to determine what cards you will likely be approved for.</p>
+            <label htmlFor="dream-title">Credit Score</label>
+            <p>If you don't know your credit score you can find it by using <a href="https://www.creditkarma.com/">credit karma</a> or looking at the score provided by your issuer. This is necessary to determine what cards you will likely be approved htmlFor.</p>
             <input type="text" name="dream-title" placeholder="Type your credit score here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">What are you trying to optimize for?</label>
-            <p>Points or cash back. If cash back is selected, cards used solely for travel will not be recommended.</p>
-            <input type="text" name="dream-title" placeholder="Type what you want to optimize for here" required/>
+            <label htmlFor="dream-title">What are you trying to optimize htmlFor?</label>
+            <p>Points or cash back. If cash back is selected, cards used solely htmlFor travel will not be recommended.</p>
+            <input type="text" name="dream-title" placeholder="Type what you want to optimize htmlFor here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">How much spend do you normally have per month and would like to put on a card?</label>
+            <label htmlFor="dream-title">How many credit cards do you currently have?</label>
+            <input type="text" name="dream-title" placeholder="Type the # of cards you have here" required/>
+          </section>
+          <section className="form-section overview-section">
+            <label htmlFor="dream-title">How much spend do you normally have per month and would like to put on a card?</label>
             <input type="text" name="dream-title" placeholder="Type how much you spend per month here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">How many non business credit cards have you gotten in the last 24 months from Chase? This affects 5/24. Link to article about 5/24</label>
+            <label htmlFor="dream-title">How many non business credit cards have you gotten in the last 24 months from Chase? This affects 5/24. Link to article about 5/24</label>
             <input type="text" name="dream-title" placeholder="Type the number here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">How many hard inquiries have you had in the last 6 months? This affects your ability to get cards from Citi.</label>
+            <label htmlFor="dream-title">How many hard inquiries have you had in the last 6 months? This affects your ability to get cards from Citi.</label>
             <input type="text" name="dream-title" placeholder="Type the number here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">Have you opened any acounts with American express in the pass?</label>
+            <label htmlFor="dream-title">Have you opened any acounts with American express in the pass?</label>
             <input type="text" name="dream-title" placeholder="Type yes no here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">Are you a student?</label>
+            <label htmlFor="dream-title">Are you a student?</label>
             <input type="text" name="dream-title" placeholder="Type yes no here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">Are there any big upcoming expenses?</label>
+            <label htmlFor="dream-title">Are there any big upcoming expenses?</label>
             <input type="text" name="dream-title" placeholder="Type yes no here" required/>
           </section>
           <section className="form-section overview-section">
-            <label for="dream-title">When was your last hard inquiry?</label>
+            <label htmlFor="dream-title">When was your last hard inquiry?</label>
             <input type="text" name="dream-title" placeholder="Type date here" required/>
           </section>
           <section className="button-section">
