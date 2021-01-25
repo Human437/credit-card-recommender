e5a1b861c675b2a1df07102738b314881f0e324a
Ryan <leeyuenhigh@gmail.com>
2021-01-25 00:22:00 -0500

    Changed to list the cards that the user has been recommended

--- src/your-cards/your-cards.js
+++ src/your-cards/your-cards.js
@@ -2,29 +2,33 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './your-cards.css'
 import RecommenderContext from './../recommenderContext'
+import STORE from './../dummy-store'
 
 export default class YourCards extends React.Component{
+  
   constructor(props){
     super(props);
     this.state={
 
     }
   }
-
-  static contextType = RecommenderContext;
+  static contextType = RecommenderContext
+  
 
   render(){
+    const user = STORE.users.find(user =>(Object.values(user).includes(this.context.userId)))
     return(
       <>
         <br/>
         <ul>
-          {this.context.availableCards.map(availableCard =>{
-            return(
-              <li key = {availableCard.id} id = {availableCard.id}>
-                <Link
-                  to = {`/cards/${availableCard.id}`}
-                >
-                  {availableCard.title}
+          {/* fetch the user based off the user id */}
+          {user.userCards.map(cardId =>{
+            // fetch the card by id
+            const card = this.context.availableCards.find(availableCard =>(Object.values(availableCard).includes(cardId)))
+            return (
+              <li key ={cardId} id = {cardId}>
+                <Link to = {`/cards/${cardId}`}>
+                  {card.title}
                 </Link>
               </li>
             )
