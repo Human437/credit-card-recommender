f6436e287094fdb4b6240a422780c4c83a2625e6
Ryan <leeyuenhigh@gmail.com>
2021-01-29 00:04:51 -0500

    Added logic for when a user has 2 CC

--- src/card-recommender/card-recommender.js
+++ src/card-recommender/card-recommender.js
@@ -128,6 +128,7 @@ export default class CardRecommender extends React.Component{
             this.context.updateUserCards([8,10])
             this.context.updateMsg('Go for the Chase Freedom Flex if you want to be more envolved in the game and take advantage of the rotating categories. If you want to have a flat 1.5% back on all purchases go for the Chas Freedom Unlimited.')
           }else{
+            this.context.updateUserCards([8,10])
             this.context.updateMsg('You should try to raise your score up to 650 before applying for either the Chase Freedom Flex or Chase Freedom Unlimited. Although, you stil may be approved with a credit score less than 650, having a score of at least 650 helps to boost your chances of being approved.')
           }
         }else{
@@ -136,6 +137,18 @@ export default class CardRecommender extends React.Component{
         }
         break;
       case 2:
+        if(this.checkLastHardInquiry(2)){
+          if(this.state.creditScore.value >= 650){
+            this.context.updateUserCards([8,10])
+            this.context.updateMsg('Go for the Chase Freedom Flex if you want to be more envolved in the game and take advantage of the rotating categories. If you want to have a flat 1.5% back on all purchases go for the Chas Freedom Unlimited. If you already have one of the Chase Freedom Cards, apply for the one that you don\'t already have.')
+          }else{
+            this.context.updateUserCards([8,10])
+            this.context.updateMsg('You should try to raise your score up to 650 before applying for either the Chase Freedom Flex or Chase Freedom Unlimited. Although, you stil may be approved with a credit score less than 650, having a score of at least 650 helps to boost your chances of being approved.')
+          }
+        }else{
+          this.context.updateUserCards([])
+          this.context.updateMsg('You should aim to leave 2 to 3 months between each credit card application so that issuers do not deny you due to you applying for too many credit cards too quickly.' )
+        }
         break;
       case 3:
         break;
