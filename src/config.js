4098db3e76f08c92362b758f5f0a0c017deb1507
Ryan <leeyuenhigh@gmail.com>
2021-02-03 01:23:57 -0500

    Created config file to read in env variables

---
+++ src/config.js
@@ -0,0 +1,7 @@
+// eslint-disable-next-line
+export default {
+  BEARER_TOKEN: process.env.REACT_APP_BEARER_TOKEN,
+  API_Articles_ENDPOINT: process.env.REACT_APP_API_Articles_ENDPOINT,
+  API_User_ENDPOINT: process.env.API_User_ENDPOINT,
+  API_Cards_ENDPOINT: process.env.API_Cards_ENDPOINT
+}
