b417f5bc060b43893ee5fac9c1c01946f7673d65
Ryan <leeyuenhigh@gmail.com>
2021-02-03 01:22:59 -0500

    Component fetches articles on it's own w/o the need for context

--- src/articles/articles.js
+++ src/articles/articles.js
@@ -2,22 +2,34 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './articles.css'
 import RecommenderContext from './../recommenderContext'
+import config from './../config'
 
 export default class Articles extends React.Component {
   constructor(props){
     super(props);
     this.state={
-
+      articles: [],
     }
   }
 
   static contextType = RecommenderContext;
 
+  componentDidMount(){
+    fetch(config.API_Articles_ENDPOINT, {
+      method: 'get',
+      headers: new Headers({
+        'Authorization': `Bearer ${config.BEARER_TOKEN}`
+      })
+    })
+    .then(response => response.json())
+    .then(data => this.setState({articles:data}))
+  }
+
   render(){
     return(
       <>
         <ul>
-          {this.context.articles.map(article =>{
+          {this.state.articles.map(article =>{
             return(
               <li key = {article.id} id = {article.id}>
                 <Link
