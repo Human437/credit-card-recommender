bf018b66a70072970928f7b6bd222cdc89d8c1f5
Ryan <leeyuenhigh@gmail.com>
2021-02-04 01:12:20 -0500

    Data for cards is now retrieved using fetch instead of dummy store

--- src/show-results/show-results.js
+++ src/show-results/show-results.js
@@ -2,27 +2,40 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './show-results.css'
 import RecommenderContext from './../recommenderContext'
+import config from './../config'
 
 export default class ShowResults extends React.Component{
   constructor(props){
     super(props);
-    this.setState={
-
+    this.state={
+      cards:[],
     }
   }
 
   static contextType =RecommenderContext
   
+  componentDidMount(){
+    let results = this.context.userCards.map(cardId =>{
+      return fetch(`${config.API_Cards_ENDPOINT}/${cardId}`, {
+        method: 'get',
+        headers: new Headers({
+          'Authorization': `Bearer ${config.BEARER_TOKEN}`
+        })
+      })
+      .then(response => response.json())
+    })
+    Promise.all(results)
+    .then(response => this.setState({cards:response}))
+  }
+  
   render(){
     return(
       <>
         <ul>
-          {this.context.userCards.map(cardId =>{
-            // fetch the card by id
-            const card = this.context.availableCards.find(availableCard =>(Object.values(availableCard).includes(cardId)))
-            return (
-              <li key ={cardId} id = {cardId}>
-                <Link to = {`/cards/${cardId}`}>
+          {this.state.cards.map(card => {
+            return(
+              <li key ={card.id} id = {card.id}>
+                <Link to = {`/cards/${card.id}`}>
                   {card.title}
                 </Link>
               </li>
