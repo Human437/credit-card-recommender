97dba7afc6af2e76d70a4a005464f48bcfce47e3
Ryan <leeyuenhigh@gmail.com>
2021-02-04 01:09:00 -0500

    Fixed config file so that it gets the right env variables

--- src/config.js
+++ src/config.js
@@ -2,6 +2,6 @@
 export default {
   BEARER_TOKEN: process.env.REACT_APP_BEARER_TOKEN,
   API_Articles_ENDPOINT: process.env.REACT_APP_API_Articles_ENDPOINT,
-  API_User_ENDPOINT: process.env.API_User_ENDPOINT,
-  API_Cards_ENDPOINT: process.env.API_Cards_ENDPOINT
+  API_User_ENDPOINT: process.env.REACT_APP_API_User_ENDPOINT,
+  API_Cards_ENDPOINT: process.env.REACT_APP_API_Cards_ENDPOINT
 }
