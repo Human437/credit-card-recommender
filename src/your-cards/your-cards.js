deb2f688b3ddc3116bc92f59651b2fb65d53b1d5
Ryan <leeyuenhigh@gmail.com>
2021-02-04 02:51:24 -0500

    Updated to use fetch to get userCards and information of each card

--- src/your-cards/your-cards.js
+++ src/your-cards/your-cards.js
@@ -2,38 +2,60 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import './your-cards.css'
 import RecommenderContext from './../recommenderContext'
-import STORE from './../dummy-store'
+import config from './../config'
 
 export default class YourCards extends React.Component{
   
   constructor(props){
     super(props);
     this.state={
-
+      cards: [],
     }
   }
   static contextType = RecommenderContext
   
+  componentDidMount(){
+    fetch(`${config.API_User_ENDPOINT}/${this.context.userId}`, {
+      method: 'get',
+      headers: new Headers({
+        'Authorization': `Bearer ${config.BEARER_TOKEN}`
+      })
+    })
+    .then(response => response.json())
+    .then(data => {
+      this.context.updateUserCards(data.usercards)
+      this.context.updateMsg(data.msg)
+      
+      let results = data.usercards.map(cardId =>{
+        return fetch(`${config.API_Cards_ENDPOINT}/${cardId}`, {
+          method: 'get',
+          headers: new Headers({
+            'Authorization': `Bearer ${config.BEARER_TOKEN}`
+          })
+        })
+        .then(response => response.json())
+      })
+      Promise.all(results)
+      .then(response => this.setState({cards:response}))
+    })
+  }
 
   render(){
-    const user = STORE.users.find(user =>(Object.values(user).includes(this.context.userId)))
     return(
       <>
         <br/>
         <ul>
-          {/* fetch the user based off the user id */}
-          {user.userCards.map(cardId =>{
-            // fetch the card by id
-            const card = this.context.availableCards.find(availableCard =>(Object.values(availableCard).includes(cardId)))
-            return (
-              <li key ={cardId} id = {cardId}>
-                <Link to = {`/cards/${cardId}`}>
+          {this.state.cards.map(card => {
+            return(
+              <li key ={card.id} id = {card.id}>
+                <Link to = {`/cards/${card.id}`}>
                   {card.title}
                 </Link>
               </li>
             )
           })}
         </ul>
+        <p>{this.context.msg}</p>
       </>
     )
   }
