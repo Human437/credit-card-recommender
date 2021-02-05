2ad5bec76c839f7d042c680ecf429ec8c98f938a
Ryan <leeyuenhigh@gmail.com>
2021-02-05 05:13:04 -0500

    Added conditional rendering for the signOn buttons

--- src/show-results/show-results.js
+++ src/show-results/show-results.js
@@ -29,6 +29,39 @@ export default class ShowResults extends React.Component{
   }
   
   render(){
+    let signOnButtons;
+    if (this.context.isSignedIn){
+      signOnButtons = <>
+                        <button
+                          onClick = {(e) => {
+                            fetch(`${config.API_User_ENDPOINT}/${this.context.userId}`, {
+                              method: 'PATCH',
+                              headers: new Headers({
+                                'Authorization': `Bearer ${config.BEARER_TOKEN}`,
+                                'content-type': 'application/json',
+                              }),
+                              body: JSON.stringify({
+                                // id: this.context.userId,
+                                usercards: this.context.userCards,
+                                msg: this.context.msg
+                              })
+                            })
+                            this.props.history.push(`/your-cards/${this.context.userId}`)
+                          }}
+                        >
+                          Update recommended cards
+                        </button>
+                      </>
+    }else{
+      signOnButtons = <>
+                        <button><Link to="/sign-up">Sign Up Today</Link></button>
+                        <button 
+                          onClick = {(e) =>{this.context.updateIsFromResultsPage(true)}}
+                        >
+                          <Link to="/sign-in">Sign In</Link>
+                        </button>
+                      </>
+    }
     return(
       <>
         <ul>
@@ -43,7 +76,8 @@ export default class ShowResults extends React.Component{
           })}
         </ul>
         <p>{this.context.msg}</p>
-        <h2><Link to="/sign-up">Save Your Results, Sign Up Today</Link></h2>
+        <h2>Save Your Results</h2>
+        {signOnButtons}
       </>
     )
   }
